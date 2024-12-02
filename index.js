62b5afca3e0183e5d175aa024c4c1e520e7c2ebe
Tushar Saini <mail.honey6401@gmail.com>
2024-12-02 15:59:11 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,7 +1,11 @@
 const express = require('express');
 const app = express();
 const port = 8080;
+const path = require('path');
 
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'ejs');
+app.set(express.static(path.join(__dirname, 'public')));
 app.use(express.json());
 
 app.get('/', (req, res) => {
