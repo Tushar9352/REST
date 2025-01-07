1364b7950681e02c6d880e6aeec22180af2268a7
Tushar Saini <mail.honey6401@gmail.com>
2025-01-07 11:10:49 +0530

    Update index.js

--- index.js
+++ index.js
@@ -3,6 +3,10 @@ const app = express();
 const path = require('path');
 const port = 8080;
 
+app.listen(port, () => {
+    console.log(`Server is running on port ${port}`);
+});
+
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
